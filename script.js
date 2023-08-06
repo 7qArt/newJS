1331bd6dae1de33b59dba3f7c8236aecc3dd9f1a
John Doe <Stilenxx@gmail.com>
2023-08-07 02:10:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+alert ( 'Я JavaScript!' );
+let message = 'hello',
+hi = 'beauty',
+darling = 'mommy';
+let admin;
+let name = 'jhon';
+admin = name;
+alert ( `hello, ${admin}` );
+name = prompt ( "what is ur name?" )
+alert (name);
