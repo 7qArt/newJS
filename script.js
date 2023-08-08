1b9d027efe4a7efd83439e9e8acc3393fa6afd5e
John Doe <Stilenxx@gmail.com>
2023-08-08 17:00:36 +0300

    nCom

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-'use strict';
-alert ( 'Я JavaScript!' );
-let message = 'hello',
-hi = 'beauty',
-darling = 'mommy';
-let admin;
-let name = 'jhon';
-admin = name;
-alert ( `hello, ${admin}` );
-name = prompt ( "what is ur name?" )
-alert (name);
+"use strict"
